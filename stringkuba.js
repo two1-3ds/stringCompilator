a7854176d60b294faa530bcdba388a1d585c8950
two1-3ds <53260353+two1-3ds@users.noreply.github.com>
2019-12-08 12:02:01 +0100

    Add files via upload

--- stringkuba.js
+++ stringkuba.js
@@ -1,3 +1,8 @@
+let tabPom=[];
+let tabC=[];
+let tabL=[];
+let lista = [];
+
 function mainFunction() {
     let txt = document.getElementById("txt").value; //pobiera string z inputa 
     let strNoNb = deleteNumbers(txt); //zamienia stringa wejściowego na string bez cyfr
@@ -6,110 +11,95 @@ function mainFunction() {
     let finalSentence = splitRest(splitShit(string));
     count2(string);
     display = showWords(arrayOfWords);
-    
-document.getElementById("result").innerHTML = display +  "<br><br>" +  finalSentence + "<br><br>" + txt.getNums().join(" ") ; // wyświetla dane rzeczy w htmlu
+    document.getElementById("result").innerHTML = display +  "<br><br>" +  finalSentence + "<br><br>" + txt.getNums().join(" ") ; // wyświetla dane rzeczy w htmlu
 }
 
-String.prototype.getNums= function(){  //wyszukuje i wypisuje wszystkie liczby niezależnie czy to zmiennoprzecinkowa czy normalna czy expo
-    var rx=/[+-]?((\.\d+)|(\d+(\.\d+)?)([eE][+-]?\d+)?)/g,
-    mapN= this.match(rx) || [];
-    console.log(mapN.map(Number));
-    return mapN.map(Number);
-    };
-
-function removeNb(nb) { //zamienia cyfry na nic
-    return nb.replace(/[0-9]/g,'');
-    }
-
-function deleteNumbers(x) { //FUNKCJA DO USUWANIA cyfr i zwraca string bez cyfr 
-    let noNbString = removeNb(x);
+function deleteNumbers(x) { //zamienia cyfry na nic i zwraca string bez cyfr 
+    let noNbString = x.replace(/[0-9]/g,'');
     console.log(x + "  -  tekst wejsciowy");
     console.log(noNbString + "  -  tekst pozbawiony cyfr");
     return noNbString;
-    }
-
-function myTrim(a) { //Trimmer z reg-exem do funkcji deleteSpaces
-  return a.replace(/^\s+|\s+/g,' ');
 }
 
 function deleteSpaces(x) { //FUNKCJA DO USUWANIA WHITESPACE'OW
-let string = myTrim(x);
-console.log(string + "  -  tekst ze zredukowanymi whitespace'ami");
-return string;
+  let string = x.replace(/^\s+|\s+/g,' ');
+  console.log(string + "  -  tekst ze zredukowanymi whitespace'ami");
+  return string;
 }
 
-function splitShit(y){  //splituje tekst do arraya, a potem robi reverse
-let splitted = y.split(" ");
-console.log(splitted + "  -  SPLITTED");
-return splitted;}
-
-function splitRest(z){
-let reversed = z.reverse();
-console.log(reversed + "  -  REVERSED");
-for(let i=0; i < reversed.length; i++){
-  if(/[A-Z]/.test(reversed[i])){
-  reversed[i] = reversed[i].toUpperCase()
-}}
-console.log(reversed + "  -  UpperCased");
-let sentence = reversed.join(" ");
-console.log(sentence + "  -  Final sentence");
-return sentence;
+function splitShit(y){  //splituje tekst do arraya
+  let splitted = y.split(" ");
+  console.log(splitted + "  -  SPLITTED");
+  return splitted;
 }
 
-let tabPom=[];
-let tabC=[];
-let tabL=[];
-let lista = [];
+function splitRest(z){ //robi reverse i dalsze operacje
+  let reversed = z.reverse();
+  console.log(reversed + "  -  REVERSED");
+    for(let i=0; i < reversed.length; i++){
+      if(/[A-Z]/.test(reversed[i])){
+        reversed[i] = reversed[i].toUpperCase()
+      }
+    }
+  console.log(reversed + "  -  UpperCased");
+  let sentence = reversed.join(" ");
+  console.log(sentence + "  -  Final sentence");
+  return sentence;
+}
 
 function count2(str){  //wrzuca duże litery do tablicy tabL
   tabPom =[]; //resetowanie tablicy
   let s = str.replace(/\s/g,'');
   let sL = s.length;
-  console.log(s);
-  console.log(sL);
-  for (i = 0; i < sL; i++) {
+  console.log(s, sL, " - ilosc znakow");
+    for (i = 0; i < sL; i++) {
       if (s.charAt(i) === s.charAt(i).toUpperCase()) {
-      tabPom.push(s.charAt(i));
+        tabPom.push(s.charAt(i));
       }
-  }
-  console.log(tabPom + " <--- TAB Pom przed petla");
-  console.log(tabC + " <--- TAB C przed petla");
-  console.log(tabL + " <--- TAB L przed petla");
-  tabC =[];
-  tabL=[];
-  for (let i=0; i<tabPom.length; i++){
-    if (tabL.includes(tabPom[i])){
-      for (let j=0; j<tabL.length; j++){
-        if (tabPom[i]===tabL[j]){
-          tabC.splice(j,1,tabC[j]+1);
+    }
+  console.log(tabPom + " <--- TAB Pom przed petla  \n\n " + tabC + " <--- TAB C przed petla \n\n " + tabL + " <--- TAB L przed petla \n" );
+  tabC =[]; //reset
+  tabL=[]; //reset
+    for (let i=0; i<tabPom.length; i++){
+      if (tabL.includes(tabPom[i])){
+        for (let j=0; j<tabL.length; j++){
+          if (tabPom[i]===tabL[j]){
+            tabC.splice(j,1,tabC[j]+1);
+          }
+          console.log(tabC + " tabC po pętli j = " + j);
         }
-        console.log(tabC + "  " + j);
-      }} 
-    else {
-    tabL.push(tabPom[i]);
-    tabC.push(+1);  
-    }    
-  console.log(tabC + "<------ TAB C po petli " + i );
-  console.log(tabL + "<------ TAB L po petli " + i ); }
+      } 
+      else{
+        tabL.push(tabPom[i]);
+        tabC.push(+1);  
+      }    
+      console.log(tabC + " <---- TAB C po petli " + i + "\n\n" + tabL + " <---- TAB L po petli " + i + "\n\n" );
+    }
 }
 
 function showWords(x){
-let array=[];
-  console.log(x);
-  console.log(tabL);
-for (let i=0; i<tabL.length; i++) {
-  array[i]=[]; //wielowymiarowa macierz w macierzy
-  for (let j=0; j<x.length; j++){
-    if (x[j].includes(tabL[i])){
-    array[i].push(x[j]);
+  let array=[];
+  console.log(x + "\n" + tabL + "\n array stringów i array dużych liter" );
+    for (let i=0; i<tabL.length; i++) {
+      array[i]=[]; 
+      for (let j=0; j<x.length; j++){
+        if (x[j].includes(tabL[i])){
+          array[i].push(x[j]);
+        }
+      }
     }
+  lista = [];
+  for (let i=0; i<tabL.length; i++) {
+    lista[i] = tabL[i].toString() + " - " + tabC[i].toString() + ",\xa0\xa0\xa0\xa0" + array[i].join(", ");
+    console.log(lista);
   }
+  let display = lista.join("<br>");
+  return display;
 }
-lista = [];
-for (let i=0; i<tabL.length; i++) {
-lista[i] = tabL[i].toString() + " - " + tabC[i].toString() + ",\xa0\xa0\xa0\xa0" + array[i].join(", ");
-console.log(lista);
-}
-let display = lista.join("<br>");
-return display;
-}
+
+String.prototype.getNums= function(){  //wyszukuje i wypisuje wszystkie liczby niezależnie czy to zmiennoprzecinkowa czy normalna czy expo
+  let rx=/[+-]?((\.\d+)|(\d+(\.\d+)?)([eE][+-]?\d+)?)/g,
+  mapN= this.match(rx) || [];
+  console.log(mapN.map(Number));
+  return mapN.map(Number);
+  }
